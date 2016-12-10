1504c9fcaef19263954a2632891ca3c85627ba85
teodoraargintaru <teodora.argintaru@outlook.com>
2016-12-10 10:09:01 +0200

    id

--- Backend/API/src/API/Models/Recipe.cs
+++ Backend/API/src/API/Models/Recipe.cs
@@ -7,26 +7,26 @@ namespace API.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string ImageURL { get; set; } 
-        public string Description { get; set; }
-        public float Rating { get; set; }
-        public float Difficulty { get; set; }
-        public List<Ingredient> Ingredients { get; set; }
-        public List<Comment> Comments { get; set; }
-        public int RequiredTime { get; set; }
-        public int Category { get; set; }
+        //public string Description { get; set; }
+        //public float Rating { get; set; }
+        //public float Difficulty { get; set; }
+        //public List<Ingredient> Ingredients { get; set; }
+        //public List<Comment> Comments { get; set; }
+        //public int RequiredTime { get; set; }
+        //public int Category { get; set; }
 
-        public Recipe(int id, string title, string imageURL, string description, float rating, float difficulty, List<Ingredient> ingredients, List<Comment> comments, int requiredTime, int category)
+        public Recipe(int id, string title, string imageURL)
         {
             Id = id;
             Title = title;
             ImageURL = imageURL;
-            Description = description;
-            Rating = rating;
-            Difficulty = difficulty;
-            Ingredients = ingredients;
-            Comments = comments;
-            RequiredTime = requiredTime;
-            Category = category;
+            //Description = description;
+            //Rating = rating;
+            //Difficulty = difficulty;
+            //Ingredients = ingredients;
+            //Comments = comments;
+            //RequiredTime = requiredTime;
+            //Category = category;
         }
     }
 }
